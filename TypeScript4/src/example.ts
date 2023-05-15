a7ad70b23d6aa7023bc1827a597d0d4e7218cceb
Akinola <adewoleakinola24@gmail.com>
2023-05-16 00:09:23 +0100

    Type Aliases

--- TypeScript4/src/example.ts
+++ TypeScript4/src/example.ts
@@ -5,9 +5,12 @@
 
 // Type Aliases can be used for primitives like string or more complex types such as objects and arrays:
 
-type carYear = number
-type carType = string
-type carModel = string
+type carYear = number;
+type carType = string;
+type carModel = string;
+type objWithName= {uid:carType, name:String};
+
+
 
 type Car = {
   year: carYear,
@@ -15,11 +18,13 @@ type Car = {
   model: carModel
 }
 
+
+
 const logDetails =(uid:carYear, item:String)=>{
     console.log(logDetails);
 }
 
-const greet =(user:{uid:carType, name:string})=>{
+const greet =(user:objWithName)=>{
     console.log(`${user.name} say Hello`);
 }
 
