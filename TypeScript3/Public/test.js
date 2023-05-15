8339062923ba6947d18357bf9048c858eb9bc8a5
Akinola <adewoleakinola24@gmail.com>
2023-05-15 22:49:05 +0100

    Any or Dynamic Type

---
+++ TypeScript3/Public/test.js
@@ -0,0 +1,8 @@
+"use strict";
+// let mixed:(Number|String|Boolean)[]=[];
+// mixed.push('Tommy Vercetti');
+// mixed.push(12345);
+// mixed.push(true);
+//15-05-2023
+//ANY TYPES
+let age = 25;
---
+++ TypeScript3/src/test.ts
@@ -0,0 +1,9 @@
+// let mixed:(Number|String|Boolean)[]=[];
+// mixed.push('Tommy Vercetti');
+// mixed.push(12345);
+// mixed.push(true);
+
+
+//15-05-2023
+//ANY TYPES
+let age:any=25;
