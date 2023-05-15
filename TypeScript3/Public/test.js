7561738bc0e4d3372c7699288c2d5307b20bc82c
Akinola <adewoleakinola24@gmail.com>
2023-05-15 22:59:37 +0100

    Dynamic or Any

--- TypeScript3/Public/test.js
+++ TypeScript3/Public/test.js
@@ -6,3 +6,16 @@
 //15-05-2023
 //ANY TYPES
 let age = 25;
+age = true;
+console.log(age);
+age = 'Yoshi';
+console.log(age);
+age = {
+    name: "Mario",
+};
+console.log(age);
+let mixed = [];
+mixed.push("Luigi");
+mixed.push(12345);
+mixed.push(true);
+console.log(mixed);
--- TypeScript3/src/test.ts
+++ TypeScript3/src/test.ts
@@ -7,3 +7,19 @@
 //15-05-2023
 //ANY TYPES
 let age:any=25;
+age=true;
+console.log(age);
+age='Yoshi';
+console.log(age);
+age={
+    name:"Mario",
+};
+console.log(age);
+
+let mixed:any[]=[];
+mixed.push("Luigi");
+mixed.push(12345);
+mixed.push(true);
+console.log(mixed);
+
+
