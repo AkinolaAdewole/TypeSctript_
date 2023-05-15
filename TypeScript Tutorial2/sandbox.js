5246fe9e8a41d0c95f12f3450665f690e086e0c5
Akinola <adewoleakinola24@gmail.com>
2023-05-15 21:55:10 +0100

    second class

---
+++ TypeScript Tutorial2/sandbox.js
@@ -0,0 +1,21 @@
+// Union Types
+var mixed = [];
+mixed.push(30);
+mixed.push('yoshi');
+mixed.push(false);
+console.log(mixed);
+var uid;
+uid = 123;
+uid = 'luigi';
+console.log(uid);
+//Objects
+var ninjaOne;
+ninjaOne = {
+    name: 'Joy',
+    age: 20,
+};
+console.log(ninjaOne);
+var ninjaTwo;
+ninjaTwo = { name: 'Ade',
+    age: 10,
+    beltColour: 'black' };
---
+++ TypeScript Tutorial2/sandbox.ts
@@ -0,0 +1,30 @@
+// Union Types
+let mixed:(string|number|boolean)[]=[];
+mixed.push(30);
+mixed.push('yoshi');
+mixed.push(false);
+console.log(mixed);
+
+let uid:number|string;
+uid=123;
+uid='luigi';
+console.log(uid);
+
+//Objects
+let ninjaOne:{};
+ninjaOne={
+    name:'Joy',
+    age:20,
+}
+console.log(ninjaOne);
+
+let ninjaTwo:{
+    name:string,
+    age:Number,
+    beltColour:string
+};
+
+ninjaTwo={name:'Ade',
+          age:10,
+          beltColour:'black'};
+
