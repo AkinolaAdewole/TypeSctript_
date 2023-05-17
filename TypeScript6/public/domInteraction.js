03d34ac57315342195b9e813f44d7b260eb73358
Akinola <adewoleakinola24@gmail.com>
2023-05-17 22:55:21 +0100

    Type Casting/DOM Interactions

--- TypeScript6/public/domInteraction.js
+++ TypeScript6/public/domInteraction.js
@@ -11,10 +11,14 @@ if (anchor) {
     console.log(anchor.href);
 }
 // Type casting
-const form = document.querySelector('.nav-item-form');
-console.log(form.children);
+const form = document.querySelector('.new-item-form');
+// console.log(form.children);
 // collect input (select field in a form)
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- TypeScript6/src/domInteraction.ts
+++ TypeScript6/src/domInteraction.ts
@@ -14,11 +14,23 @@ if(anchor){
 }
 
 // Type casting
-const form = document.querySelector('.nav-item-form') as HTMLFormElement;
-console.log(form.children);
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
 
 // collect input (select field in a form)
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e:Event)=>{
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber
+    );
+    
+})
